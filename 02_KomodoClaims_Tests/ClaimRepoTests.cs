5f8001e177af872b732683bb8eabb99d2ac19499
tuganbraden <tuganbraden@gmail.com>
2020-12-17 17:23:13 -0500

    Added Claims test project

---
+++ 02_KomodoClaims_Tests/ClaimRepoTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using _02_KomodoClaims_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _02_KomodoClaims_Tests
+{
+    [TestClass]
+    public class ClaimRepoTests
+    {
+        [TestMethod]
+        public void AddClaim_ShouldGetNotNull()
+        {
+            Claim test = new Claim();
+            ClaimRepo testRepo = new ClaimRepo();
+
+            testRepo.AddClaim(test);
+            Claim fromRepo = testRepo.GetQueue().Peek();
+
+            Assert.IsNotNull(fromRepo);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Claim test = new Claim();
+            Claim test2 = new Claim();
+            ClaimRepo testRepo = new ClaimRepo();
+
+            testRepo.AddClaim(test);
+            testRepo.AddClaim(test2);
+            testRepo.HandleClaim();
+
+            Assert.AreEqual(test2, testRepo.GetQueue().Peek());
+        }
+    }
+}
---
+++ 02_KomodoClaims_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("02_KomodoClaims_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("02_KomodoClaims_Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac1c0321-eb7e-4726-b096-83c17e6b9d3a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
